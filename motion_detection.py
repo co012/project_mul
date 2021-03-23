9c8c7a93e18c5dac8d48a0bf45cceaa72169a072
co012 <co123@interia.eu>
2021-03-23 21:07:03 +0100

    extract debug control

--- motion_detection.py
+++ motion_detection.py
@@ -2,9 +2,11 @@ import cv2
 import argparse
 import numpy as np
 import detection_mask_controller as dmc
+import debug_controller as dc
 
 AGH_CAM = "http://live.uci.agh.edu.pl/video/stream1.cgi"
-SOME_VIDEO = "https://static.vecteezy.com/system/resources/previews/001/806/954/mp4/two-square-rotation-white-rectangle-animation-free-video.mp4"
+SOME_VIDEO = "https://static.vecteezy.com/system/resources/" \
+             "previews/001/806/954/mp4/two-square-rotation-white-rectangle-animation-free-video.mp4"
 DEFAULT_MIN_AREA = 200  # area of smallest possible contour
 MAIN_WINDOW_NAME = "main_window"
 
@@ -23,14 +25,15 @@ print("area of smallest contour = ", minArea)
 
 mode = args.get("mode", "normal")
 print("mode = ", mode)
-showDebugInfo = "DEBUG" == mode.upper()
-
 
 cv2.namedWindow(MAIN_WINDOW_NAME)
 capture = cv2.VideoCapture(videoSource)
 wasReadSuccessful, referenceImage = capture.read()
 backgroundSubtractor = cv2.bgsegm.createBackgroundSubtractorGSOC()
 detectionMaskController = dmc.DetectionMaskController(referenceImage, MAIN_WINDOW_NAME)
+debugController = dc.DebugController()
+if mode == "Debug":
+    debugController.active_debug_mode(referenceImage)
 
 while capture.isOpened():
     wasReadSuccessful, image = capture.read()
@@ -49,27 +52,20 @@ while capture.isOpened():
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0))
 
-    images = [grayscaleImage, reducedNoiseGrayscaleImage, foregroundMask,
-              detectionMaskController.mask, foregroundMaskWithDetectionSpace]
-    height = int(image.shape[0] * 0.25)
-    width = int(images[0].shape[1] * height / images[0].shape[0])
-    imagesResized = [cv2.resize(im, (width,height), interpolation=cv2.INTER_CUBIC) for im in images]
-    debugWindow = cv2.vconcat(imagesResized)
-
     if detectionMaskController.can_rectangle_be_drawn():
         cv2.rectangle(image, *detectionMaskController.points, (0, 0, 255), 2)
 
-    if showDebugInfo:
-        cv2.imshow("debug", debugWindow)
-        cv2.moveWindow("debug", int(2.1 * image.shape[0]), 110)
+    cv2.imshow(MAIN_WINDOW_NAME, image)
+    images = [grayscaleImage, reducedNoiseGrayscaleImage, foregroundMask,
+              detectionMaskController.mask, foregroundMaskWithDetectionSpace]
 
-    cv2.imshow("main_window", image)
+    if debugController.debugIsActive:
+        debugController.show_debug_window(images)
 
-    k = cv2.waitKey(1)
-
-    if k == ord('d'):
-        showDebugInfo = True
-    if k == ord('q'):
+    key = cv2.waitKey(1)
+    if key == ord('d'):
+        debugController.toggle_debug(image)
+    if key == ord('q'):
         break
 
     # if cv2.waitKey(1) & 0xFF == ord('q'):
