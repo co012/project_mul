5ff9a962d926082c54da15eaa4933bd0089b8e71
zofiagrodecka <69384237+zofiagrodecka@users.noreply.github.com>
2021-06-08 17:58:59 +0200

    Better for loop

--- jpeg/src/pl/edu/agh/kis/jpeg/DCT.java
+++ jpeg/src/pl/edu/agh/kis/jpeg/DCT.java
@@ -12,23 +12,19 @@ class DCT {
     /**
      * Quantitization Matrix for luminace.
      */
-    private final int[] quantum_luminance =
-            new int[]{16, 11, 10, 16, 24, 40, 51, 61, 12, 12, 14, 19, 26, 58, 60, 55, 14,
+    private final int[] quantum_luminance = { 16, 11, 10, 16, 24, 40, 51, 61, 12, 12, 14, 19, 26, 58, 60, 55, 14,
                     13, 16, 24, 40, 57, 69, 56, 14, 17, 22, 29, 51, 87, 80, 62, 18, 22, 37,
                     56, 68, 109, 103, 77, 24, 35, 55, 64, 81, 104, 113, 92, 49, 64, 78, 87,
-                    103, 121, 120, 101, 72, 92, 95, 98, 112, 100, 103, 99,
-    };
+                    103, 121, 120, 101, 72, 92, 95, 98, 112, 100, 103, 99 };
     private final double[] DivisorsLuminance = new double[N * N];
 
     /**
      * Quantitization Matrix for chrominance.
      */
-    private final int[] quantum_chrominance =
-            new int[]{17, 18, 24, 47, 99, 99, 99, 99, 18, 21, 26, 66, 99, 99, 99, 99, 24, 26,
+    private final int[] quantum_chrominance = { 17, 18, 24, 47, 99, 99, 99, 99, 18, 21, 26, 66, 99, 99, 99, 99, 24, 26,
                     56, 99, 99, 99, 99, 99, 47, 66, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99,
                     99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99, 99,
-                    99, 99, 99, 99, 99, 99, 99, 99
-    };
+                    99, 99, 99, 99, 99, 99, 99, 99 };
     private final double[] DivisorsChrominance = new double[N * N];
 
     /* Constructs a new DCT object. Initializes the cosine transform matrix these
@@ -50,8 +46,6 @@ class DCT {
     private void initMatrix(final int quality) {
         double[] AANscaleFactor = {1.0, 1.387039845, 1.306562965, 1.175875602, 1.0, 0.785694958, 0.541196100,
                 0.275899379};
-        int i;
-        int j;
         int index;
         int qualityNormalized;
         int temp;
@@ -68,40 +62,38 @@ class DCT {
 
 
 
-        for (j = 0; j < 64; j++) {
+        for (int j = 0; j < N*N; j++) {
             temp = (quantum_luminance[j] * qualityNormalized + 50) / 100;
             quantum_luminance[j] = Math.max(Math.min(temp, 255), 1);
         }
         index = 0;
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 //The divisors for the LL&M method (the slow integer method used in
 //jpeg 6a library).  This method is currently (04/04/98) incompletely
 //implemented.
 //                     DivisorsLuminance[index] = ((double) quantum_luminance[index]) << 3;
 //The divisors for the AAN method (the float method used in jpeg 6a library.
-                DivisorsLuminance[index] = (1.0
-                        / ((double) quantum_luminance[index] * AANscaleFactor[i] * AANscaleFactor[j] * 8.0));
+                DivisorsLuminance[index] = 1.0 / (quantum_luminance[index] * AANscaleFactor[i] * AANscaleFactor[j] * 8.0);
                 index++;
             }
         }
 
 
-        for (j = 0; j < 64; j++) {
+        for (int j = 0; j < N*N; j++) {
             temp = (quantum_chrominance[j] * qualityNormalized + 50) / 100;
             temp = Math.max(Math.min(temp, 255), 1);
             quantum_chrominance[j] = temp;
         }
         index = 0;
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 //The divisors for the LL&M method (the slow integer method used in
 //jpeg 6a library).  This method is currently (04/04/98) incompletely
 //implemented.
 //                     DivisorsChrominance[index] = ((double) quantum_chrominance[index]) << 3;
 //The divisors for the AAN method (the float method used in jpeg 6a library.
-                DivisorsChrominance[index] = (1.0
-                        / ((double) quantum_chrominance[index] * AANscaleFactor[i] * AANscaleFactor[j] * 8.0));
+                DivisorsChrominance[index] = 1.0 / (quantum_chrominance[index] * AANscaleFactor[i] * AANscaleFactor[j] * 8.0);
                 index++;
             }
         }
@@ -122,21 +114,17 @@ class DCT {
     double[][] forwardDCT(float[][] input) {
         double[][] output = new double[N][N];
         double tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+        double[] tmp = new double[N];
         double tmp10, tmp11, tmp12, tmp13;
         double z1, z2, z3, z4, z5, z11, z13;
-        int i;
-        int j;
 
-//Subtracts 128 from the input values
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
-                output[i][j] = ((double) input[i][j] - 128.0);
-//                     input[i][j] -= 128;
-
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                output[i][j] = input[i][j] - 128.0;
             }
         }
 
-        for (i = 0; i < 8; i++) {
+        for (int i = 0; i < N; i++) {
             tmp0 = output[i][0] + output[i][7];
             tmp7 = output[i][0] - output[i][7];
             tmp1 = output[i][1] + output[i][6];
@@ -176,7 +164,7 @@ class DCT {
             output[i][7] = z11 - z4;
         }
 
-        for (i = 0; i < 8; i++) {
+        for (int i = 0; i < N; i++) {
             tmp0 = output[0][i] + output[7][i];
             tmp7 = output[0][i] - output[7][i];
             tmp1 = output[1][i] + output[6][i];
@@ -224,11 +212,10 @@ class DCT {
      */
     int[] quantizeBlock(double[][] inputData, int code) {
         int[] outputData = new int[N * N];
-        int i, j;
         int index;
         index = 0;
-        for (i = 0; i < 8; i++) {
-            for (j = 0; j < 8; j++) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
 //The second line results in significantly better compression.
                 outputData[index] = (int) (Math.round(inputData[i][j] * (((double[]) (Divisors[code]))[index])));
 //                     outputData[index] = (int)(((inputData[i][j] * (((double[]) (Divisors[code]))[index])) + 16384.5) -16384);
